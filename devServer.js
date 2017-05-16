1a3605fe439c3cc2ae5dca639168470d373e8f84
Jeesun Kim <jeesunkim@JEESUNs-MBP.fios-router.home>
2017-05-15 20:07:38 -0400

    slack > successfully getting firebase custom token

--- devServer.js
+++ devServer.js
@@ -8,96 +8,117 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.dev');
 
 // firebase
-const firebase = require('firebase');
+// const firebase = require('firebase');
 const firebaseConfig = require('./firebase');
+
+// firebase admin
 const serviceAccount = require('./fb-movie-night-service-account.json');
+const admin = require("firebase-admin");
+const adminConfig = {
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: firebaseConfig.config.databaseURL
+};
+
+// firebase var
+let firebaseToken;
 
 const request = require('request');
 const colors = require('colors');
 const app = express();
 const compiler = webpack(webpackConfig);
-
-let user = firebase.auth().currentUser;
-
-console.log(user, ' user');
-
-// console.log(user, 'LOGGED IN USER');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
-// const createFirebaseToken = (slackID) => {
-//   // The uid we'll assign to the user.
-//   const uid = `slack:${slackID}`;
-//   console.log(uid);
-//   // Create the custom token.
-//   return firebase.auth().createCustomToken(uid);
-// }
+app.use(cookieParser());
+
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: firebaseConfig.config.databaseURL
+});
 
 app.use(webpackDevMiddleware(compiler, {
-  path: path.resolve(__dirname, "/public/build"),
-  index: 'index.html'
+   path: path.resolve(__dirname, "/public/build"),
+   index: 'index.html'
 }));
-
-// app.get('/auth', function(req, res) {
-//   res.json({ test: 'successful!' });
-// });
 
 app.use(webpackHotMiddleware(compiler));
 
-// app.use(express.static(__dirname + '/public'));
-
-// app.use(function(req, res) {
-  // console.log(req, 'REQ from app.use');
-  // console.log(res, 'RES from app.use');
-  // res.sendFile(__dirname + '/public/index.html');
-// });
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/auth', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/authenticated', (req, res) => {
+   res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/auth/redirect', (req, res) => {
-    const options = {
-        uri: 'https://slack.com/api/oauth.access?code='
-            +req.query.code+
-            '&client_id='+process.env.SLACK_CLIENT_ID+
-            '&client_secret='+process.env.SLACK_CLIENT_SECRET+
-            '&redirect_uri='+process.env.SLACK_REDIRECT_URI,
-        method: 'GET'
-    };
-    request(options, (error, response, body) => {
-        const JSONresponse = JSON.parse(body)
-        console.log(colors.yellow(JSON.stringify(JSONresponse)), 'JSONresponse');
-        if (!JSONresponse.ok){
-            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
-        }else{
-            console.log(colors.red(JSONresponse.access_token));
 
-            const firebaseToken = createFirebaseToken(JSONresponse.user.id);
+   const options = {
+      uri: 'https://slack.com/api/oauth.access?code='
+      +req.query.code+
+      '&client_id='+process.env.SLACK_CLIENT_ID+
+      '&client_secret='+process.env.SLACK_CLIENT_SECRET+
+      '&redirect_uri='+process.env.SLACK_REDIRECT_URI,
+      method: 'GET'
+   };
 
-            console.log(firebaseToken, ' firebaseToken');
+   request(options, (error, response, body) => {
+      const JSONresponse = JSON.parse(body)
+      console.log(colors.yellow(JSON.stringify(JSONresponse)), 'JSONresponse');
+      if (!JSONresponse.ok){
+         res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+      }else{
 
-            // base.post(`users/${JSONresponse.user.id}`, {
-            //   data: {
-            //     name: JSONresponse.user.name, 
-            //     avatar: JSONresponse.user.image_32
-            //   },then(err){
-            //     if(!err){
-            //       res.redirect('/');
-            //       user = JSONresponse.user.name;
-            //       console.log(colors.green(user), 'user in I am not an error section');
-            //     }
-            //   }
-            // });
+         const slackAccessToken = JSONresponse.access_token;
+         const userID = JSONresponse.user.id;
+         const userName = JSONresponse.user.name;
+         const userPic = JSONresponse.user.image_32;
 
-        };
-    });
+         return admin.auth().createCustomToken(userID)
+            .then((customToken) => {
+
+               // var tempApp = firebaseConfig.firebase.initializeApp(firebaseConfig.config, '_temp_');
+
+               console.log(customToken, ' customToken')
+
+               // return customToken;
+
+               // res.render('/', {firebaseToken: customToken});
+
+               // res.redirect('/').json({
+               //    status: "success",
+               //    token: customToken
+               // })
+            });
+      };
+   });
 });
 
+// var tempApp = firebaseConfig.firebase.initializeApp(firebaseConfig.config, '_temp_');
+// tempApp.auth().signInWithCustomToken(firebaseToken).then(function(user) {
+
+//    const tasks = [tempApp.database().ref('/SlackAccessToken/' + user.uid)
+//    .set('${slackAccessToken}')];
+
+//    if ('${userName}' !== user.displayName || '${profilePic}' !== user.photoURL) {
+//       tasks.push(user.updateProfile({displayName: '${userName}', photoURL: '${profilePic}'}));
+//    }
+
+//    return Promise.all(tasks).then(function() {
+//       var defaultApp = firebase.initializeApp(firebaseConfig.config);
+
+//       console.log(defaultApp, ' defaultApp')
+//       Promise.all([
+//          defaultApp.auth().signInWithCustomToken(firebaseToken),
+//          tempApp.delete()]).then(function() {
+//             window.close(); 
+//          });
+//       });
+// });
+
+console.log(firebaseToken, ' firebaseToken')
+
 app.listen(3333, () => {
-  console.log("Listening on port 3333!");
+   console.log("Listening on port 3333!");
 });
