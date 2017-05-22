5041265bfad3a1854b7bab3c97934ff1f1ff6687
Jeesun Kim <jeesun.kim@nyc6fbjkim.americas.media.global.loc>
2017-05-22 11:06:20 -0400

    slack > moving firebase api to env although I don't have to

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 const firebase = require('firebase');
+require('dotenv').config();
 
 const config = {
-	apiKey: "AIzaSyAU80pmT8LYBN5lelCa4uyzoLgAHEJH-5E",
+	apiKey: process.env.FIREBASE_API_KEY,
 	authDomain: "fb-movie-night.firebaseapp.com",
 	databaseURL: "https://fb-movie-night.firebaseio.com",
 	projectId: "fb-movie-night",
 	storageBucket: "fb-movie-night.appspot.com",
-	messagingSenderId: "296065424169"
+	messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID
 };
 
 firebase.initializeApp(config);
