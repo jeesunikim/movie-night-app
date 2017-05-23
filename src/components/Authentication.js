39b1f795c2461e9686d4f352863ac090fb3efc85
Jeesun Kim <jeesunkim@JEESUNs-MBP.fios-router.home>
2017-05-22 23:20:32 -0400

    slack > uid set state added

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -7,10 +7,15 @@ class Autentication extends React.Component {
 	constructor() {
 		super();
 
+		this.authenticate = this.authenticate.bind(this);
 		this.logout = this.logout.bind(this);
-		this.login = this.login.bind(this);
+		this.renderLogin = this.renderLogin.bind(this);
 
-		let userName, userPhoto, userID;
+		this.state = {
+			uid: null
+		}
+
+		// let userName, userPhoto, userID;
 
 		console.log(Firebase.auth(), ' Firebase.auth()');
 
@@ -25,7 +30,20 @@ class Autentication extends React.Component {
 
 	}
 
-	login () {
+	componentDidMount() {
+		Firebase.auth().onAuthStateChanged((user) => {
+			if(user) {
+				console.log('user exists');
+			}
+		});
+
+
+				// if(user !== null) {
+
+					
+	}
+	authenticate () {
+		console.log(' I am so authenticate');
 		fetch('/api', {
 
 			method: 'GET'
@@ -48,11 +66,17 @@ class Autentication extends React.Component {
 				if(user !== null) {
 
 					user.updateProfile({
+						uid: json.userID,
 						displayName: json.userName,
 						photoURL: json.userPic
+
 					}).then(() => {
 
-						console.log('user != null');
+						this.setState({
+							uid: json.userID
+						})
+
+						console.log('user != null', this.state);
 
 					}, (error) => {
 
@@ -72,20 +96,32 @@ class Autentication extends React.Component {
 
 	}
 
-
 	logout () {
 		Firebase.auth().signOut().then(() =>{
 			console.log('success');
+			this.setState({ uid: null })
 		})
 	}
 
-	render () {
+	renderLogin () {
 		return (
-			<div>
-				<a onClick={this.logout}>logout</a>
-				<a onClick={this.login}>login</a>
-			</div>
+			<nav className="login">
+				<p>Sign in to vote or submit a movie</p>
+				<a onClick={this.authenticate}>login</a>
+				<a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=3992851480.155742621031"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+			</nav>
 		)
+	}
+
+	render () {
+		const logout = <button onClick={this.logout}>Log out!</button>
+
+		// check if they are logged in
+		if(!this.state.uid) {
+			return <div>{this.renderLogin()}</div>
+		}else{
+			return <div>{logout}</div>
+		}
 	}
 
 };
