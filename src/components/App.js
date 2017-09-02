f5ae31178071993eb4cf926b13084055e099cc4e
Jeesun Kim <jeesunkim@JEESUNs-MacBook-Pro.local>
2017-09-02 18:09:36 -0400

    feature/list-movies > refactored ListMovie.js, deleted the old upvote code; basic styling for list added

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,10 @@ import Authentication from './Authentication';
 import firebaseConfig from '../../firebase';
 import * as firebase from 'firebase';
 
-
 class App extends React.Component {
 	constructor () {
 		super();
 		this.addMovie = this.addMovie.bind(this);
-		this.upvoteMovie = this.upvoteMovie.bind(this);
 		this.removeMovie = this.removeMovie.bind(this);
 
 		// initial state
@@ -40,21 +38,17 @@ class App extends React.Component {
 		const newMovieRef = this.firebaseRef.push();
 	
 		this.setState({movies});
+
+		console.log(movie, ' movie')
 	
 		newMovieRef.set({
-			desc: movie.desc,
 			imageUrl: movie.imageUrl,
+			imdbID: movie.imdbID,
 			likes: movie.likes,
 			name: movie.name,
 			createdBy: firebaseConfig.auth.currentUser.uid,
 			key: newMovieRef.key
 		});
-	}
-
-	upvoteMovie (key, upvotedMovie) {
-		const movies = {...this.state.movies};
-		movies[key] = upvotedMovie;
-		this.setState({movies});
 	}
 
 	removeMovie (key) {
@@ -66,32 +60,19 @@ class App extends React.Component {
 	
 	render () {
 		return (
-			<div className="movie-night">
+			<div className="MovieApp">
 				<Authentication />
+				<h1>Firstborn Movie Night</h1>
+				<AddMovieForm addMovie={this.addMovie} />
 				{this.state.isLoaded && 
-					<Carousel 
+					<Carousel movies={this.state.movies} />
+				}
+				{this.state.isLoaded && 
+					<ListMovie
 						movies={this.state.movies} 
+						removeMovie={this.removeMovie}
 					/>
 				}
-				<div className="movie-night__wrapper">
-					<h2>What movie should we watch this month?</h2>
-					<AddMovieForm addMovie={this.addMovie} />
-					<ul className="movie-night__list">
-						{
-							Object.keys(this.state.movies)
-							.map(key => 
-								<ListMovie 
-									key={key} 
-									index={key}
-									movies={this.state.movies} 
-									details={this.state.movies[key]} 
-									upvoteMovie={this.upvoteMovie} 
-									removeMovie={this.removeMovie}
-								/>
-							)
-						}
-					</ul>
-				</div>
 			</div>
 		)
 	}
