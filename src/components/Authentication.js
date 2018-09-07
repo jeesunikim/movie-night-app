cd7545ba3df12aa0c64e9d673a173d7f5d02f7b5
Jeesun Kim <jeesunkim@JEESUNs-MBP.fios-router.home>
2018-09-06 23:02:17 -0400

    bugfix/login_no_refresh > logging no longer only happens with /authentication; using async/await

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Firebase from 'firebase';
-import firebaseConfig from '../../firebase';
+import React from "react";
+import axios from "axios";
+import Firebase from "firebase";
+import firebaseConfig from "../../firebase";
 
 class Autentication extends React.Component {
-
 	constructor() {
 		super();
 
-		this.authenticate = this.authenticate.bind(this);
 		this.logout = this.logout.bind(this);
 		this.renderLogin = this.renderLogin.bind(this);
 
@@ -15,108 +14,118 @@ class Autentication extends React.Component {
 			uid: null,
 			userName: null,
 			userPhoto: null
-		}
-
+		};
 	}
 
 	componentDidMount() {
-		if(window.location.pathname === '/authenticated') {
-			this.authenticate();
-		}
+		this.slackAuth();
 	}
 
 	componentWillUnmount() {
-		firebaseConfig.auth.signOut().then(() =>{
-			this.setState({ uid: null })
-		})
+		firebaseConfig.auth.signOut().then(() => {
+			this.setState({ uid: null });
+		});
 	}
 
-	authenticate () {
-		fetch('/api/users', {
+	async slackAuth() {
+		try {
+			const response = await axios.get("/api/users");
+			const userData = await response.data;
 
-			method: 'GET'
-			
-		}).then((res) => {
+			this.firebaseAuth(userData);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-			return res.json();
-
-		}).then((json) => {
-
-			console.log(json, ' json')
-
-
-			firebaseConfig.auth.signInWithCustomToken(json.firebaseToken).catch((error) => {
+	firebaseAuth(userData) {
+		firebaseConfig.auth
+			.signInWithCustomToken(userData.firebaseToken)
+			.catch(error => {
 				var errorCode = error.code;
 				var errorMessage = error.message;
 			});
 
-			const unsubscribe = firebaseConfig.auth.onAuthStateChanged((user) => {
+		const unsubscribe = firebaseConfig.auth.onAuthStateChanged(user => {
+			unsubscribe();
 
-				unsubscribe();
+			if (user) {
+				const userRef = firebaseConfig.database.ref(
+					"/users/" + firebaseConfig.auth.currentUser.uid
+				);
 
-				if(user) {
-
-					const userRef = firebaseConfig.database.ref("/users/" + firebaseConfig.auth.currentUser.uid);
-
-					// userRef.push();
-
-					userRef.set({
-						username: json.userName,
-						photoURL: json.userPic
-					}).then(() =>{
-						console.log('Synchronization succeeded');
-					}).catch(() => {
-						console.log('Synchronization failed');
-					});	
-
-					this.setState({
-						uid: firebaseConfig.auth.currentUser.uid,
-						userName: json.userName,
-						userPhoto: json.userPic
+				userRef
+					.set({
+						username: userData.userName,
+						photoURL: userData.userPic
+					})
+					.then(() => {
+						console.log("Synchronization succeeded");
+					})
+					.catch(() => {
+						console.log("Synchronization failed");
 					});
-				};
-			});		
 
+				this.setState({
+					uid: firebaseConfig.auth.currentUser.uid,
+					userName: userData.userName,
+					userPhoto: userData.userPic
+				});
+			}
 		});
 	}
 
-	logout () {
-		firebaseConfig.auth.signOut().then(() =>{
-			this.setState({ uid: null })
-		})
+	logout() {
+		firebaseConfig.auth.signOut().then(() => {
+			this.setState({ uid: null });
+		});
 	}
 
-	renderLogin () {
-
+	renderLogin() {
 		return (
 			<nav className="login">
 				<p>Sign in to vote or submit a movie</p>
-				<a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=3992851480.155742621031"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+				<a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=3992851480.155742621031">
+					<img
+						alt="Sign in with Slack"
+						height="40"
+						width="172"
+						src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+						srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+					/>
+				</a>
 			</nav>
-		)
+		);
 	}
 
-	render () {
-		const logout = <button onClick={this.logout}>Log out!</button>
+	render() {
+		const logout = <button onClick={this.logout}>Log out!</button>;
 
 		// check if they are logged in
-		if(!this.state.uid) {
+		if (!this.state.uid) {
 			return (
 				<div className="MovieApp__authentication">
 					<span>{this.renderLogin()}</span>
 				</div>
-			)
-		}else{
+			);
+		} else {
 			return (
 				<div className="MovieApp__authentication">
-					<span>Hello <strong>{this.state.userName}</strong></span>
-					<img className="avatar_image" src={this.state.userPhoto} alt="user's slack picture" width="32px" height="32px" />
+					<span>
+						Hello <strong>{this.state.userName}</strong>
+					</span>
+					<img
+						className="avatar_image"
+						src={this.state.userPhoto}
+						alt="user's slack picture"
+						width="32px"
+						height="32px"
+					/>
 					{logout}
 				</div>
-			)
+			);
 		}
 	}
-
-};
+}
 
 export default Autentication;
