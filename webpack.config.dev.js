d52dbda54093e54fc016d9ba1fccc83211f274ea
jeesunikim <jeesunikim@gmail.com>
2019-02-18 23:47:54 -0500

    feature/slideshow > slideshow added; updated babel

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,52 +5,53 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-	filename: "bundle.css"
+  filename: "bundle.css"
 });
 
 module.exports = {
-	devtool: "source-map",
-	context: path.resolve(__dirname, "src"),
-	entry: [
-		"webpack-hot-middleware/client?reload=true",
-		"index.js",
-		"./styles/styles.scss"
-	],
-	output: {
-		path: path.resolve(__dirname, "public/build"),
-		publicPath: "/build",
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: [path.resolve(__dirname, "./src")],
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				options: {
-					presets: ["es2015", "react"],
-					plugins: ["transform-object-rest-spread"]
-				}
-			},
-			{
-				test: /\.(sass|scss)$/,
-				loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
-			},
-			{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract({
-					loader: "css-loader?importLoaders=1"
-				})
-			}
-		]
-	},
-	plugins: [extractSass, new webpack.HotModuleReplacementPlugin()],
-	resolve: {
-		modules: [
-			"node_modules",
-			path.resolve("src", "components"),
-			path.resolve("src")
-		]
-	}
+  devtool: "source-map",
+  context: path.resolve(__dirname, "src"),
+  entry: [
+    "@babel/polyfill",
+    "webpack-hot-middleware/client?reload=true",
+    "index.js",
+    "./styles/styles.scss"
+  ],
+  output: {
+    path: path.resolve(__dirname, "public/build"),
+    publicPath: "/build",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "./src")],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader?importLoaders=1"
+        })
+      }
+    ]
+  },
+  plugins: [extractSass, new webpack.HotModuleReplacementPlugin()],
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve("src", "components"),
+      path.resolve("src")
+    ]
+  }
 };
